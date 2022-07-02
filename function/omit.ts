a20a15cf9ba1941aab3cacd9b3089c697e6a9d4d
clinfc <1962109568@qq.com>
2022-07-02 10:45:35 +0800

    refactor: 类型优化

--- function/omit.ts
+++ function/omit.ts
@@ -1,5 +1,10 @@
-export function omit<T, K extends keyof T>(data: T, ...keys: K[]) {
-  const temp: Omit<T, OrFromArrayLike<typeof keys>> = { ...data }
+/**
+ * 对 TypeScript 类型推导 Omit 的 JavaScript 实现
+ * @param data 数据源
+ * @param keys 被剔除的键
+ */
+export function omit<T, K extends keyof T>(data: T, keys: K[]) {
+  const temp: Omit<T, ArrayValudOf<typeof keys>> = { ...data }
   keys.forEach((key) => Reflect.deleteProperty(temp, key))
   return temp
 }
--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,15 @@
-declare type OrFromArrayLike<T> = T extends ArrayLike<infer U> ? U : never
+/**
+ * 从对象中获取值类型
+ */
+declare type ValueOf<T extends object, K = keyof T> = K extends keyof T ? T[K] : never
+
+/**
+ * 从数组中获取值类型
+ */
+declare type ArrayValudOf<T> = T extends ArrayLike<infer U> ? U : never
 
 /**
  * 空值合并
  * @see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
  */
 declare type Nullish<T, D> = T extends undefined | null ? D : T
-
-/**
- * keyof 的对立面，取值的类型
- */
-declare type ValueOf<T extends object, K = keyof T> = K extends keyof T ? T[K] : never
